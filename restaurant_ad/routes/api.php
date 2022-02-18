73c88bc3bd201d83fe5bb1d870baa1c1285d9f3c
theo deletang <theo@MacBook-Pro-de-theo.local>
2022-02-18 14:49:44 +0100

    ajout de route dans le api.php

--- restaurant_ad/routes/api.php
+++ restaurant_ad/routes/api.php
@@ -17,3 +17,47 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register'), function(login, password, email, name, firstname, age){
+
+};
+
+Route::post('/auth'), function(login, password){
+
+};
+
+Route::get('/users'), function(){
+
+};
+
+Route::get('/restaurants'), function(){
+
+};
+
+Route::post('/restaurant'), function(name, description, grade, localization, phone_number, website, hours){
+
+};
+
+Route::put('/restaurant/{id}'), function(name, description, grade, localization, phone_number, website, hours){
+
+};
+
+Route::delete('restaurant/{id}'), function(){
+
+};
+
+Route::get('/restaurant/{id}/menus'), function(){
+
+};
+
+Route::post('/restaurant/{id}/menu/{id}'), function(name, description, price){
+
+};
+
+Route::put('/restaurant/{id}/menu{id}'), function(name, description, price){
+
+};
+
+Route::delete('/restaurant/{id}/menu/{id}'), function(){
+
+};
