0ab5241aeddfc65b4e7a5ad0267aca8aef4ab805
theo deletang <theo@MacBook-Pro-de-theo.local>
2022-02-21 16:24:55 +0100

    htm de conexion et inscription

---
+++ restaurant_ad/resources/views/inscri.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="../style.css">
+	<title>inscription restaurant advisor</title>
+</head>
+
+<body>
+	<h2>restaurant advisor</h2>
+	<fieldset>
+		<legend>inscription</legend>
+		<p>name</p>
+		<input type="name" name="name">
+		<p>firstname</p>
+		<input type="firstname" name="firstname">
+		<p>username</p>
+		<input type="username" name="username">
+		<p>email</p>
+		<input type="email" name="email">
+		<p>password</p>
+		<input type="password" name="password"><br>
+		<button>sign up</button>
+		<button><a href="/conexion">already have an account</a></button>
+	</fieldset>
+</body>
+</html>
